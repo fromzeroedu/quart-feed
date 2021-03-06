a5c5ccf8c9d41703759f45c62fb59894c08dc12a
Jorge Escobar <jorge@fromzero.io>
2021-03-06 18:39:43 -0500

    completed step-2

--- user/views.py
+++ user/views.py
@@ -1,10 +1,25 @@
-from quart import Blueprint, render_template
+from quart import Blueprint, render_template, request
 
 from user.models import user_table
 
 user_app = Blueprint("user_app", __name__)
 
 
-@user_app.route("/register")
-async def register():
-    return await render_template("user/register.html")
+@user_app.route("/register", methods=["GET", "POST"])
+async def register() -> str:
+    error: str = ""
+    username: str = ""
+    password: str = ""
+    if request.method == "POST":
+        form = await request.form
+        username = form.get("username", "")
+        password = form.get("password", "")
+
+        if not username or not password:
+            error = "Please enter username and password"
+        else:
+            # check if the user exists
+            # register the user
+            pass
+
+    return await render_template("user/register.html", error=error, username=username)

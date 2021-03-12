dcae6251ccf20e635f4545962665c7d963d6bd3d
Jorge Escobar <jorge@fromzero.io>
2021-03-12 15:53:18 -0500

    completed step-3

--- user/models.py
+++ user/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Table, Column, Integer, String
+from aiomysql.sa.connection import SAConnection
 
 from db import metadata
 
@@ -10,3 +11,15 @@ user_table = Table(
     Column("password", String(128)),
     Column("image", String(45), nullable=True),
 )
+
+
+async def get_user_by_username(conn: SAConnection, username: str) -> dict:
+    user_query = user_table.select().where(user_table.c.username == username)
+    user_row = await conn.fetch_one(query=user_query)
+
+    if user_row:
+        user_dict = dict(user_row)
+    else:
+        return {}
+
+    return user_dict
--- user/views.py
+++ user/views.py
@@ -1,15 +1,32 @@
-from quart import Blueprint, render_template, request
+from quart import (
+    Blueprint,
+    render_template,
+    request,
+    session,
+    current_app,
+    flash,
+    redirect,
+    url_for,
+)
+from quart.wrappers.response import Response
+from passlib.hash import pbkdf2_sha256
+import uuid
 
-from user.models import user_table
+from user.models import user_table, get_user_by_username
 
 user_app = Blueprint("user_app", __name__)
 
 
 @user_app.route("/register", methods=["GET", "POST"])
-async def register() -> str:
+async def register() -> Response:
     error: str = ""
     username: str = ""
     password: str = ""
+    csrf_token: uuid.UUID = uuid.uuid4()
+
+    if request.method == "GET":
+        session["csrf_token"] = str(csrf_token)
+
     if request.method == "POST":
         form = await request.form
         username = form.get("username", "")
@@ -18,8 +35,40 @@ async def register() -> str:
         if not username or not password:
             error = "Please enter username and password"
         else:
-            # check if the user exists
-            # register the user
-            pass
+            if (
+                session.get("csrf_token") != form.get("csrf_token")
+                and not current_app.testing
+            ):
+                error = "Invalid POST contents"
 
-    return await render_template("user/register.html", error=error, username=username)
+            conn = current_app.dbc
+
+            # check if the user exists
+            if not error:
+                user = await get_user_by_username(conn, username)
+                if user and user["id"]:
+                    error = "Username already exists"
+
+            # register the user
+            if not error:
+                if not current_app.testing:
+                    del session["csrf_token"]
+
+                hash: str = pbkdf2_sha256.hash(password)
+                user_insert = user_table.insert().values(
+                    username=username, password=hash
+                )
+                await conn.execute(query=user_insert)
+                await flash("You have been registered, please login")
+                return redirect(url_for(".login"))
+            else:
+                session["csrf_token"] = str(csrf_token)
+
+    return await render_template(
+        "user/register.html", error=error, username=username, csrf_token=csrf_token
+    )
+
+
+@user_app.route("/login", methods=["GET", "POST"])
+async def login() -> str:
+    return "Login page"
